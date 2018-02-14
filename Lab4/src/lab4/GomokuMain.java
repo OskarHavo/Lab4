c2f638d72c624dea4f803dc692ca9d33582ae018
Oskar <Oskar@130.240.92.172>
2018-02-14 09:11:23 +0100

    Main typ klar

--- Lab4/src/lab4/GomokuMain.java
+++ Lab4/src/lab4/GomokuMain.java
@@ -1,8 +1,27 @@
 package lab4;
 
+import lab4.client.GomokuClient;
+import lab4.data.GomokuGameState;
+import lab4.gui.GomokuGUI;
+
 public class GomokuMain {
-	public GomokuMain() {
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int port;
+		if(args.length==1) {
+			//argument
+			port = Integer.parseInt(args[0]);
 		
+		}else {
+			//default
+			port = 4000;
+		}
+		GomokuClient client = new GomokuClient(port);
+		GomokuGameState gameState = new GomokuGameState(client);
+		GomokuGUI gui = new GomokuGUI(gameState,client);
 		
 	}
 
